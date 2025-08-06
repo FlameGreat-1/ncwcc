333bf738024e7ce6a6a30de29a792ada8a073d29
Iziogo Emmanuel Ugochukwu <eugochukwu77@gmail.com>
2025-08-06 15:18:17 +0100

    fixed import issue

--- src/pages/clients/Portal.jsx
+++ src/pages/clients/Portal.jsx
@@ -1,5 +1,5 @@
-import { useAuth } from '../contexts/AuthContext';
-import { useTheme } from '../contexts/ThemeContext';
+import { useAuth } from '../../contexts/AuthContext';
+import { useTheme } from '../../contexts/ThemeContext';
 import { Link } from 'react-router-dom';
 
 const Portal = () => {
